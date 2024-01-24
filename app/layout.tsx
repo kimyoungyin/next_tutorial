2930f3bdffeaaf9c907dbc672414228e3435987b
kimyoungyin <yin199859@gmail.com>
2024-01-24 15:53:31 +0900

    add Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
 import { inter } from '@/app/ui/fonts';
 import '@/app/ui/global.css';
+import { Metadata } from 'next';
+
+// 자동으로 이게 해당 페이지의 Metadata로 설정됨
+export const metadata: Metadata = {
+  // %s는 페이지에서 선언한 title로 대체됨
+  title: { template: '%s | Acme Dashboard', default: 'Acme Dashboard' },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
